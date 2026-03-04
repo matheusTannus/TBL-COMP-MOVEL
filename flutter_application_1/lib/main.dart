81196397ee618caf61eab8b7be2517aed2d84c92
matheus Tannus <matheustannusdesousa@gmail.com>
2026-03-04 12:04:31 -0300

    Atividade 1 Rot2

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Tela1());
+  }
+}
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Você está na tela inicial")),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Ir para detalhe"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Tela2()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Você está na Tela de Detalhe")),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Voltar"),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
